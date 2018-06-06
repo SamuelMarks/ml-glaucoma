3a17e567a7d36277069d64a86e10645e5834e3ef
Samuel Marks <samuelmarks@gmail.com>
2018-06-07 00:37:42 +1000

    Cleaning up `epoch2stat`
    Former-commit-id: 5c942a98571f8fed74bd914917d9cbd983e37de0

--- ml_glaucoma/CNN/bmes_cnn.py
+++ ml_glaucoma/CNN/bmes_cnn.py
@@ -43,18 +43,20 @@ def parser(infile, top, threshold, by_diff):
         key: val
         for key, val in iteritems(
         {
-            k: tuple((lambda stats: stats)(tuple(imap(itemgetter(1), v))))
-            for k, v in groupby(sorted(imap(lambda l: (l[0], l[1]),
-                                            ifilter(None, imap(
-                                                lambda l: (lambda fst: (
-                                                    lambda three: (int(three), l.rstrip()[l.rfind(':') + 2:])
-                                                    if three is not None and three.isdigit() and int(
-                                                        three[0]) < 4 else None)(
-                                                    l[fst - 3:fst] if fst > -1 else None))(l.rfind(']')), infile)
-                                                    )), key=itemgetter(0)),
-                                itemgetter(0))
+            k: tuple(imap(itemgetter(1), v))
+            for k, v in groupby(  # sorted
+            (imap(lambda l: (l[0], l[1]),
+                  ifilter(None, imap(
+                      lambda l: (lambda fst: (
+                          lambda three: (int(three), l.rstrip()[l.rfind(':') + 2:])
+                          if three is not None and three.isdigit() and int(
+                              three[0]) < 4 else None)(
+                          l[fst - 3:fst] if fst > -1 else None))(l.rfind(']')), infile)
+                          ))  # , key=itemgetter(0)
+             ), itemgetter(0))
         })
         if val and len(val) == 2
+
     }
 
     if threshold is not None:
@@ -63,7 +65,6 @@ def parser(infile, top, threshold, by_diff):
                 lambda val: float(''.join(takewhile(lambda c: c.isdigit() or c == '.', val[::-1]))[::-1]),
                 v))
              ) for k, v in iteritems(epoch2stat)), key=itemgetter(1))
-
         pp(tuple(islice((epoch2stat[k[0]] for k in within_threshold if k[1]), 0, top)))
     elif by_diff:
         lowest_diff = sorted((
