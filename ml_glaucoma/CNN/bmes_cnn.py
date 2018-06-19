7cc360397b8c6edcab027034c85f4f12c80e5145
Samuel Marks <samuelmarks@gmail.com>
2018-06-19 13:23:07 +1000

    Less of previous arguments to fit_generator
    Former-commit-id: bd1c355675f5b400040cd0b65fdceafe6731f97e

--- ml_glaucoma/CNN/bmes_cnn.py
+++ ml_glaucoma/CNN/bmes_cnn.py
@@ -232,8 +232,7 @@ def run(download_dir, bmes123_pardir, preprocess_to, batch_size, num_classes, ep
     model.compile(loss=getattr(keras.losses, loss),
                   optimizer=getattr(keras.optimizers, optimizer)() if optimizer in dir(keras.optimizers) else optimizer,
                   metrics=metrics)
-    model.fit_generator(train_seq, validation_data=valid_seq, epochs=epochs, callbacks=callbacks, verbose=1,
-                        batch_size=batch_size)
+    model.fit_generator(train_seq, validation_data=valid_seq, epochs=epochs, callbacks=callbacks, verbose=1)
     score = model.evaluate_generator(test_seq, verbose=0)
 
     '''model.fit(x_train, y_train,
