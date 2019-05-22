c2425ed8f66d04e6038e67747ead7833f26928d4
Dominic Jack <thedomjack@gmail.com>
2019-05-23 09:35:04 +1000

    Applied hack to only hack-required versions of tensorflow_datasets
    Former-commit-id: aedd53bce37ab49f353ad3c781f1173e9aaadcf2

--- ml_glaucoma/tfds_checksums.py
+++ ml_glaucoma/tfds_checksums.py
@@ -1,6 +1,8 @@
 """
 Hacky work-around to https://github.com/tensorflow/datasets/issues/580
 
+Resolved in master branch, though required for earlier versions.
+
 Note there's a @memoize on `tfds.core.download.checksums._checksum_paths()`,
 so this should be imported before any other `tfds` usage.
 """
@@ -8,9 +10,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+try:
 import os
-import tensorflow_datasets as tfds
-
-
-tfds.core.download.checksums._CHECKSUM_DIRS.append(
-    os.path.realpath(os.path.join(os.path.dirname(__file__), 'url_checksums')))
+    import tensorflow_datasets as tfds
+    tfds.core.download.checksums._CHECKSUM_DIRS.append(os.path.realpath(
+        os.path.join(os.path.dirname(__file__), 'url_checksums')))
+    tfds.core.download.checksums._checksum_paths.cache_clear()
+except AttributeError:
+    # later versions of tfds don't have tfds.core.download.checksums
+    # bug seems fixed in these?
+    pass
