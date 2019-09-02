15587a04d45df8731829a59d41ab2bd4f08a5d35
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-09-02 13:21:28 +1000

    [*.py] Completing Keras to tf.keras conversion
    Former-commit-id: 6b8d9a29aa74f6698fe373f3e730328f2bbf1c1a

--- ml_glaucoma/CNN/metrics.py
+++ ml_glaucoma/CNN/metrics.py
@@ -3,8 +3,8 @@ from platform import python_version_tuple
 import tensorflow as tf
 from tensorflow.keras import backend as K
 from tensorflow.keras.callbacks import Callback
-from tensorflow.keras.engine import Layer
 from sklearn.metrics import precision_recall_fscore_support, fbeta_score
+from tensorflow.python.keras.engine import Layer
 
 from ml_glaucoma.CNN.helpers import output_sensitivity_specificity
 
