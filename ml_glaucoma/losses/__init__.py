613db08d76a741d28aea4ac0c07078eb79208eaa
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-09-09 21:09:38 +1000

    [DiceLoss] Added new loss function
    Former-commit-id: 85764d568bb11982095291c5b7635b8b663eff43

--- ml_glaucoma/losses/__init__.py
+++ ml_glaucoma/losses/__init__.py
@@ -1,3 +1,4 @@
 from ml_glaucoma.losses.binary_crossentropy_with_ranking import BinaryCrossentropyWithRanking
+from ml_glaucoma.losses.dice_loss import DiceLoss
 from ml_glaucoma.losses.pair_loss import PairLoss
 from ml_glaucoma.losses.soft_auc import SoftAUC
