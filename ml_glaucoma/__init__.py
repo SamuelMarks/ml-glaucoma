f3aab29d019f5ef8ba90922a3fcd538f39892ef7
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-09-22 18:50:51 +1000

    [losses] Added Smooth L1
    Former-commit-id: 840413b020a5e70f6047713c2570ff66d7817bf3

--- ml_glaucoma/__init__.py
+++ ml_glaucoma/__init__.py
@@ -10,7 +10,7 @@ import ml_glaucoma.tf_compat
 import ml_glaucoma.tfds_checksums
 
 __author__ = 'Samuel Marks'
-__version__ = '0.0.50'
+__version__ = '0.0.51'
 
 
 def get_logger(name=None):
--- ml_glaucoma/losses/__init__.py
+++ ml_glaucoma/losses/__init__.py
@@ -3,4 +3,5 @@ from ml_glaucoma.losses.dice_loss import DiceLoss
 from ml_glaucoma.losses.jaccard_distance import JaccardDistance
 from ml_glaucoma.losses.kappa_loss import Kappa
 from ml_glaucoma.losses.pair_loss import PairLoss
+from ml_glaucoma.losses.smooth_l1 import SmoothL1
 from ml_glaucoma.losses.soft_auc import SoftAUC
