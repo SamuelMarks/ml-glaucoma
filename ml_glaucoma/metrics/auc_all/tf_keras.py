9de956e5f438e28afe0db6beaa28b6ac0ab368e7
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-01-02 13:12:02 +1100

    [ml_glaucoma/metrics/auc_all/tf_keras.py] Set `name`

--- ml_glaucoma/metrics/auc_all/tf_keras.py
+++ ml_glaucoma/metrics/auc_all/tf_keras.py
@@ -6,6 +6,20 @@ class AUCall(tf.keras.metrics.AUC):
     tf.keras.metrics.AUC but also emit TP, FP, TN, FN
     """
 
+    def __init__(self,
+                 num_thresholds=200,
+                 curve='ROC',
+                 summation_method='interpolation',
+                 name='AUCall',
+                 dtype=None,
+                 thresholds=None):
+        super(AUCall, self).__init__(num_thresholds=num_thresholds,
+                                     curve=curve,
+                                     summation_method=summation_method,
+                                     name=name,
+                                     dtype=dtype,
+                                     thresholds=thresholds)
+
     def result(self):
         tf.summary.scalar('auc_tp', data=self.true_positives)
         tf.summary.scalar('auc_fp', data=self.false_positives)
