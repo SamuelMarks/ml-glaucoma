1fc47c6db68356da62f0f248e40b1d6ac968e04f
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-01-04 08:43:06 +1100

    [data_preparation_scripts/pipeline.sh] Consistent variables

--- data_preparation_scripts/pipeline.sh
+++ data_preparation_scripts/pipeline.sh
@@ -3,13 +3,14 @@
 data_dir="${DATA_DIR:-"$HOME"/tensorflow_datasets}"
 transfer_gin="$(python -c 'from pkg_resources import resource_filename; from os import path; print(path.join(path.dirname(resource_filename("ml_glaucoma", "__init__.py")), "model_configs", "transfer.gin"))')"
 manual_dir="${MANUAL_DIR:-/mnt}"
+dataset="${DATASET:-dr_spoc}"
 
 python -m ml_glaucoma pipeline \
     --options="{'losses':    [{'BinaryCrossentropy': 0}, {'JaccardDistance': 0}],
                 'optimizer': {Adam: 5, RMSProp: 10}]}" \
     --strategy='grid|random|biggest-grid|smallest-grid|bayes|genetic|raytune' \
     train \
-    -ds 'refuge' \
+    -ds "$dataset" \
     --data_dir "$data_dir" \
     --model_file "$transfer_gin" \
     --model_dir "$manual_dir"'/gon_MobileNet_epochs_5' \
@@ -24,7 +25,7 @@ python -m ml_glaucoma pipeline \
     --threshold '1' \
     --logfile "$manual_dir"'/pipeline.log' \
     --dry-run 'train' \
-    -ds 'refuge' \
+    -ds "$dataset" \
     --data_dir "$data_dir" \
     --model_file "$transfer_gin" \
     --model_dir "$manual_dir"'/gon_MobileNet_epochs_test' \
