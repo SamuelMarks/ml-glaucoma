54696504eac76c7fd59b9f8047458d920b2bf5fd
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-01-19 19:09:37 +1100

    [gcloud_scripts/test.bash] curl straight from demo

--- gcloud_scripts/test.bash
+++ gcloud_scripts/test.bash
@@ -2,14 +2,14 @@
 
 declare -r DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
+if [ -z "$NETWORK" ]; then
+  printf '$NETWORK not defined; try including all environment variables specified in README.md\n' >&2
+  exit 2
+fi
+
 set -euov pipefail
 
 declare -r TPU_ADDR="$(gcloud compute tpus describe $TPU_NAME --format='value[separator=":"](networkEndpoints.ipAddress, networkEndpoints.port)')"
-
-gcloud compute scp --zone "$ZONE" --project="$PROJECT_ID" \
-                   "$DIR"'/tpu-tester.py'  "$INSTANCE":~/
-
-printf '$TPU_ADDR = "%s"' "$TPU_ADDR"
 
 gcloud compute ssh "$INSTANCE" \
        --command='( dpkg -s python3-venv &> /dev/null || sudo apt-get install -y python3-venv ) &&
@@ -17,6 +17,10 @@ gcloud compute ssh "$INSTANCE" \
                   . ~/venv/bin/activate &&
                   ( python -c "import pkgutil; exit(int(pkgutil.find_loader(\"tensorflow\") is not None))" &&
                     pip3 install -U pip setuptools wheel &&
-                    pip3 install tensorflow tensorflow-datasets ||
+                    pip3 install tensorflow tensorflow-datasets nbconvert ||
                     true ) &&
+                  ( [ -f tpu-tester.py ] ||
+                    curl -sL https://raw.githubusercontent.com/tensorflow/docs/bef6f89/site/en/guide/tpu.ipynb |
+                    jupyter nbconvert --to script --stdin --output tpu-tester.py &&
+                    sed "s/%/pass \#/g" tpu-tester.py.txt > tpu-tester.py ) &&
                   COLAB_TPU_ADDR="'"$TPU_ADDR"'" python tpu-tester.py'
