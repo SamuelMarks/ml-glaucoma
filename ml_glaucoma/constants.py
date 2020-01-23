c38ca7016feada3ec582a5a4d35d44147b36ca89
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-01-23 21:07:04 +1100

    [constants] __doc__ is read-only

--- ml_glaucoma/constants.py
+++ ml_glaucoma/constants.py
@@ -1,7 +1,8 @@
 from os import path
 
 SAVE_FORMAT = 'h5'
-SAVE_FORMAT.__doc__ = """
+#SAVE_FORMAT.__doc__ =
+"""
 Either 'tf' or 'h5', indicating whether to save the model
         to Tensorflow SavedModel or HDF5. Meant to default to 'tf' in TF 2.X, and 'h5' in TF 1.X.
       ImportError: If save format is hdf5, and h5py is not available
